7e12c7b1a75b3b4157308e239ca28d0cbd9559e1
Frederic Chauchon <fchauchon@micropole.com>
2022-10-15 12:11:20 +0200

    day1

--- README.md
+++ README.md
@@ -25,5 +25,3 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
-
-<span *ngIf="false" class="w3-padding"><img src="assets/logo.svg" width="100px" /></span>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { DataComponent } from './data/data.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    DataComponent,
+    SideBarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
