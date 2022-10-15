e11d09b42750773171b6672b67a08d96d120f8da
Frederic Chauchon <fchauchon@micropole.com>
2022-10-15 13:18:58 +0200

    WIP

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
-  styleUrls: ['./data.component.css']
+  styleUrls: ['./data.component.css'],
 })
-export class DataComponent implements OnInit {
+export class DataComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    isHidden: boolean = false;
+    title: string = 'data'
+    titleClass: any = { 'font-style': 'italic' }
 
-  ngOnInit(): void {
-  }
+    body: string = 'Je suis le body'
+    bodyStyles: any = {}
 
+    inputValue: string = ''
+    
+    constructor() {
+        console.warn('Constructor')
+    }
+
+    ngOnInit(): void {
+        this.titleClass.gras = true;
+        this.inputValue = 'Input value';
+    }
+
+    onClick() {
+        this.isHidden = ! this.isHidden
+    }
+
+    ngOnDestroy(): void {
+    }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    logoPath: string = 'assets/logo.svg'
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
