d4f711c9e6058be8f7cfa6caa533ae23e78b2985
Frederic Chauchon <fchauchon@micropole.com>
2022-10-26 18:39:33 +0200

    Forms

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,34 @@
+import { FormControl, FormGroup, UntypedFormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { combineLatest } from 'rxjs';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    form: UntypedFormGroup
+    password: FormControl<string | null>
+    confirmPassword: FormControl<string | null>
+    percent = 0
 
-  ngOnInit(): void {
-  }
+    constructor() {
+        this.password = new FormControl<string>('')
+        this.confirmPassword = new FormControl<string>('')
+        this.form = new UntypedFormGroup(
+            {
+                password: this.password,
+                confirmPassword: this.confirmPassword
+            }
+        )
+    }
+
+    ngOnInit(): void {
+        combineLatest([this.password.valueChanges, this.confirmPassword.valueChanges]).subscribe(
+            (val) => console.log(val)
+        )
+    }
 
 }
