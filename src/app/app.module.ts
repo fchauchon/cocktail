3f0856e1ad5b23f39a7270534d9ab216ff067325
Frederic Chauchon <fchauchon@micropole.com>
2022-11-11 14:00:04 +0100

    day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { CocktailComponent } from './cocktail/cocktail.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
 import { MainComponent } from './main/main.component';
 
 @NgModule({
@@ -18,7 +17,6 @@ import { MainComponent } from './main/main.component';
     FooterComponent,
     BodyComponent,
     CocktailComponent,
-    SideBarComponent,
     MainComponent
   ],
   imports: [
