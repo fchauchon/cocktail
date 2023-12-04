82b73e08c88e2981dab1752f0ec150f46c150190
CHAUCHON Frédéric <fchauchon@micropole.com>
2023-12-04 10:28:53 +0100

    Ajout des valeurs statiques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ styleUrls: ['./app.component.css']
 })
 export class AppComponent {
     title = 'cocktail';
+
+    // cocktails: any[] = []
+    // this.cocktails.push( { name: 'Mojito', description: 'Rien de mieux qu\'un bon mojito maison fait dans les régles de l\'art', img: 'assets/mojito.jpg', alcohol: true } )
+    // this.cocktails.push( { name: 'Cuba libre', description: 'Le Cuba libre est un cocktail officiel de l\'IBA4, à base de rhum, citron vert, et cola.', img: 'assets/cubalibre.jpg', alcohol: true } )
+    // this.cocktails.push( { name: 'Margarita', description: 'La Margarita est un cocktail à base de tequila, inventé par des Américains au Mexique', img: 'assets/margarita.jpg', alcohol: true } )
+    // this.cocktails.push( { name: 'Sex on the beach', description: 'Le Sex on the Beach est un cocktail alcoolisé contenant de la vodka, du Schnaps à la pêche, du jus d\'orange et du jus de canneberge', img: 'assets/sexonthebeach.jpg', alcohol: true } )
+    // this.cocktails.push( { name: 'Virgin Mojito', description: 'Le Virgin Mojito est inspiré par le célèbre Mojito cubain, l\'un des ceux qui représente le plus la culture cubaine, à l\'égal du Cuba libre et du Daiquiri.', img: 'assets/virginmojito.jpg', alcohol: false } )
+    // this.cocktails.push( { name: 'Halloween Punch', description: 'Pour en mettre plein les yeux le soir le plus effrayant de l\'année, préparez un punch d\'Halloween facile… couleur rouge sang et rempli de globes oculaires !', img: 'assets/holloweenpunch.jpg', alcohol: false } )
 }
